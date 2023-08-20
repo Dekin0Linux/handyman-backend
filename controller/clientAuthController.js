19c5452d3d41774d6ce3f6a5db788359d20ae23a
dekin <dekinlinux@gmail.com>
2023-08-20 12:06:37 +0000

    backend client setup

---
+++ controller/clientAuthController.js
@@ -0,0 +1,108 @@
+const clientAuthModel = require("../models/clientAuthModel")
+const brcrypt = require("bcryptjs")
+
+
+// GET ALL CLIENTS
+const getClients = async (req,res)=>{
+    const clients = await clientAuthModel.find()
+    try{
+        if(!clients){
+            res.json({msg:"Sorry Bad request"})
+        }else[
+            res.json(clients)
+        ]
+    }catch(err){
+        res.send(err)
+    }
+}
+
+// GET SINGLE CLIENT
+const getSingleClient = async (req,res)=>{
+    const {clientId} = req.params
+
+    const clients = await clientAuthModel.findOne({id:clientId})
+    try{
+        if(!clients){
+            res.json({msg:"No client found"})
+        }else{
+            res.json(clients)
+        }
+    }catch(err){
+        res.send(err)
+    }
+}
+
+// ADDING NEW CLIENT
+const newClient = async(req,res)=>{
+    const data = req.body
+    try{
+        const addNew = await clientAuthModel.create(data)
+        if(!addNew){
+            return res.status("409").json({msg:"User not created"})
+        }
+        res.json(addNew).status(200)
+    }catch(err){
+        res.json({msg:err.message})
+    }
+    res.json(data)
+}
+
+//loggin in a client
+const login = async(req,res)=>{
+    const {email,password} = req.body
+
+    try{
+        const loginUser = await clientAuthModel.where("email",email).where("password",password)
+        if(!loginUser){
+            return res.status("409").json({msg:"User not created"})
+        }
+        res.json(loginUser).status(200)
+    }catch(err){
+        res.json({msg:err.message})
+    }
+    res.json(data)
+}
+
+//EDIT UESR INFO
+const updateClient = async (req,res)=>{
+    const data = req.body
+    const {clientId} = req.params
+    
+    try{
+        const clients = await clientAuthModel.updateOne({_id:clientId},data)
+        if(!clients){
+            res.json({msg:"No client found"})
+        }else{
+            res.json(clients)
+        }
+    }catch(err){
+        res.json({msg:err.message})
+    }
+}
+
+
+// DELETE CLIENT
+const deleteClient = async (req,res)=>{
+    const {clientId} = req.params
+    try{
+        const client = await clientAuthModel.findByIdAndDelete({_id:clientId})
+        if(!client){
+            res.json({msg:"No client found"})
+        }else{
+            res.json({msg:"remove successfully"});
+            return true
+        }
+    }catch(err){
+        res.json({msg:err.message})
+    }
+}
+
+
+module.exports = {
+    getClients,
+    getSingleClient,
+    newClient,
+    login,
+    updateClient,
+    deleteClient
+}
