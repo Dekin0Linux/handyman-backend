c6d6c37a30bd51b5da4a1231bb52ad934106c8c4
Ali Abdul Basit <aliabdulbasit2002@gmail.com>
2023-09-01 00:21:04 +0000

    changes

--- models/RequestModel.js
+++ models/RequestModel.js
@@ -1,47 +1,53 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-
-const requestSchema = mongoose.Schema({
-    business : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'business',
-        require : true
+const requestSchema = mongoose.Schema(
+  {
+    business: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "business",
+      require: true,
     },
-    client : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'client',
-        require : true
+    client: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "client",
+      require: true,
     },
-    requestDate : {
-        type: String,
-        required : true
+    requestDate: {
+      type: String,
+      required: true,
     },
-    address : {
-        type : String,
-        required : true,
+    address: {
+      type: String,
+      required: true,
     },
-    phone : {
-        type : String,
-        required : true,
+    phone: {
+      type: String,
+      required: true,
     },
     location: {
-        type : Object,
-        default : null,
+      type: Object,
+      default: null,
     },
-    description:{
-        type : String,
-        required : true
+    description: {
+      type: String,
+      required: true,
     },
-    refNumber : {
-        type : String,
-        require : true
+    refNumber: {
+      type: String,
+      require: true,
     },
-    requestStatus : {
-        type : String,
-        default : 'pending',
-        // enum : ['accepted','pending','cancelled','paid','completed'],
+    requestStatus: {
+      type: String,
+      default: "pending",
+      // enum : ['accepted','pending','cancelled','paid','completed'],
     },
-},{timestamps : true})
+    status: {
+      type: String,
+      default: "pend",
+    },
+  },
+  { timestamps: true }
+);
 
-const requestModel = mongoose.model('request',requestSchema)
+const requestModel = mongoose.model("request", requestSchema);
 module.exports = requestModel;
