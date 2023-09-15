c2b7f16afa094150e1c30efee4481d01a3008d97
dekin <dekinlinux@gmail.com>
2023-09-15 16:58:07 +0000

    image files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const path = require("path");
+const path = require("path") ;
 
 // ROUTERS
 const clientAuth = require("./routes/clientAuth");
